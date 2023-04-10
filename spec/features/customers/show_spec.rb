d62c015c46cb569847fda6a123de5caba75c457f
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-10 11:23:24 -0500

    Test: Update customer feature spec and rails helper

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -7,23 +7,23 @@
 # including the item's name, price, and the name of the supermarket that it belongs to.
 require 'rails_helper'
 
-RSpec.describe 'Customer Show Page' do
-  let!(supermarket_1) { Supermarket.create!(name: 'Piggly Wiggly', location: "Austin, TX") }
-  let!(supermarket_2) { Supermarket.create!(name: 'King Stupor', location: "Denver, CO") }
-  let!(item_1) { supermarket_1.items.create!(name: 'Tofurky Slices', price: 3) }
-  let!(item_2) { supermarket_1.items.create!(name: 'Chao Cheese Slices', price: 5) }
-  let!(customer_1) { Customer.create!(name: 'Billy Bob') }
-  let!(customer_2) { Customer.create!(name: 'Gary Garble') }
+RSpec.describe 'Customer Show Page', type: :features do
+  let!(:supermarket_1) { Supermarket.create!(name: 'Piggly Wiggly', location: "Austin, TX") }
+  let!(:supermarket_2) { Supermarket.create!(name: 'King Stupor', location: "Denver, CO") }
+  let!(:item_1) { supermarket_1.items.create!(name: 'Tofurky Slices', price: 3) }
+  let!(:item_2) { supermarket_1.items.create!(name: 'Chao Cheese Slices', price: 5) }
+  let!(:customer_1) { Customer.create!(name: 'Billy Bob') }
+  let!(:customer_2) { Customer.create!(name: 'Gary Garble') }
 
   describe 'displays customer attributes' do
     it 'should display the name of the customer' do
-      visit "/customer/#{customer_1.id}"
+      visit "/customers/#{customer_1.id}"
       
       expect(page).to have_content(customer_1.name)
     end
     
     it 'should display a list of items, including price, belonging to the customer and where they came from.' do
-      visit "/customer/#{customer_1.id}"
+      visit "/customers/#{customer_1.id}"
 
       expect(page).to have_content(item_1.name)
       expect(page).to have_content(item_1.price)
